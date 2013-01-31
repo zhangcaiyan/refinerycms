bf2fc267ca4fd587eba35c4572dc99dbaa2eb664
zhangcaiyan <zhangcaiyanbeyond@gmail.com>
2013-01-31 16:08:18 +0800

    domain中添加users获得该域名下的用户和管理员功能

--- core/app/models/refinery/core/domain.rb
+++ core/app/models/refinery/core/domain.rb
@@ -4,7 +4,12 @@ module Refinery
   module Core
     class Domain < ActiveRecord::Base
 
+      has_many :users, class_name: "Refinery::User", foreign_key: :domain_id
 
+      def user
+        role = Refinery::Core::Role.find_by_title_and_domain_id("Superuser", id)
+        role.users.first
+      end
 
     end
   end
